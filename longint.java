d964f877ac56f309c9aa5b4d05c394c1011512ff
vina786 <vinay76b@gmail.com>
2024-12-12 14:54:20 +0530

    Add files via upload

---
+++ longint.java
@@ -0,0 +1,14 @@
+//prints upto its range of long
+import java.util.*;
+public class longint {
+    public static void main(String args[])
+    {
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();//long s=sc.nextInt();
+        System.out.println(ans);
+        System.out.println(s);
+
+    }
+    
+}
