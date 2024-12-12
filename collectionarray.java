c4a578c429338033575b4d5379b664ea5d9ad4cc
vina786 <vinay76b@gmail.com>
2024-12-12 12:20:56 +0530

    Add files via upload

---
+++ collectionarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class collectionarray {
+public static void main(String args[]){
+    //arrays collections sort
+    ArrayList<Integer> a=new ArrayList<>();
+    a.add(45);
+    a.add(1);
+    a.add(18);
+    a.add(12);
+    Collections.sort(a);
+    System.out.println(a);
+    
+}
+}
