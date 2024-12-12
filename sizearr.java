17e45bdab36c7558820e352eb079c8f6a803b50b
vina786 <vinay76b@gmail.com>
2024-12-12 22:08:59 +0530

    Add files via upload

---
+++ sizearr.java
@@ -0,0 +1,15 @@
+// size of the given array
+import java.util.*;
+public class sizearr {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(898);
+        a.add(89);
+        a.add(1);
+        a.add(98);
+        System.out.println(a.size());
+        
+    }
+    
+}
