4af59c20c7b971221aea6528af26e482cb0077f4
vina786 <vinay76b@gmail.com>
2024-12-12 22:08:22 +0530

    Add files via upload

---
+++ convertingarr1.java
@@ -0,0 +1,17 @@
+//converting elements into array
+import java.util.*;
+public class convertingarr1
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(898);
+        a.add(89);
+        a.add(1);
+        a.add(78);
+        int b[]=new int[a.size()];
+        for(int i=0;i<a.size();i++)
+        b[i]=a.get(i);
+        System.out.println(Arrays.toString(b)); 
+}
+}
